4e1ea2021bc9dca8f3086f503fd585aeb4994629
vinayaviator95 <vinayaviator95@gmail.com>
2023-04-10 00:49:09 +0530

    ErrorProps methods

---
+++ src/components/TextError.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TextError = (props) => {
+  return <div className="errors-content">{props.children}</div>;
+};
+
+export default TextError;
--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import "../styles/youtubeForm.css";
 import * as Yup from "yup";
+import TextError from "./TextError";
 
 const initialValues = {
   name: "",
@@ -35,25 +36,35 @@ const YoutubeForm = () => {
         <label className="yt-label" htmlFor="name">
           Name
         </label>
-        <Field className="yt-input" type="text" id="name" name="name" />
+        <Field
+          className="yt-input"
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Enter your name"
+        />
         <div className="errors-content">
-          <ErrorMessage name="name" />
+          {/* errormessage with custome component for renderting the error */}
+          <ErrorMessage name="name" component={TextError} />
         </div>
         <label className="yt-label" htmlFor="email">
           E-mail
         </label>
         <Field className="yt-input" type="email" id="email" name="email" />
-        <div className="errors-content">
-          <ErrorMessage name="email" />
-        </div>
+        {/* we can passs any element in the component to wrap it with error message to the compoennt */}
+        <ErrorMessage name="email" component="Div" />
 
         <label className="yt-label" htmlFor="channel">
           Channel
         </label>
         <Field className="yt-input" type="text" name="channel" id="channel" />
-        <div className="errors-content">
-          <ErrorMessage name="channel" />
-        </div>
+        {/* Showing error with method renderor prop pattern */}
+        <ErrorMessage name="channel">
+          {(errorMsg) => {
+            return <div className="errors-content">{errorMsg}</div>;
+          }}
+        </ErrorMessage>
+
         <Field as="textarea" name="comments" id="comments" />
         <button type="submit" className="yt-btn" onClick={() => {}}>
           Submit
